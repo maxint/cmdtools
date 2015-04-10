e0859fbf13e4887e5b6ad1676cb7e10f541c67e1
maxint <lnychina@gmail.com>
2015-04-10 15:37:47 +0800

    Add subtree.py

---
+++ subtree.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Git subtree manage commands.
+"""
+
+import subprocess
+
+
+def run(cmd):
+    print cmd
+    return subprocess.check_call(cmd)
+
+
+def do_add(args):
+    run('git remote add -f {}.git {}'.format(args.prefix, args.url))
+    run('git subtree add --prefix {0} {0}.git master --squash'.format(args.prefix))
+
+
+def do_push(args):
+    run('git subtree push --prefix {0} {0}.git master --squash'.format(args.prefix))
+    do_pull(args)
+
+
+def do_pull(args):
+    run('git subtree pull --prefix {0} {0}.git master'.format(args.prefix))
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Convienient operations for git subtree command')
+    subparsers = parser.add_subparsers()
+
+    addp = subparsers.add_parser('add')
+    addp.add_argument('--prefix')
+    addp.add_argument('url', help='repository location')
+    addp.set_defaults(func=do_add)
+
+    pushp = subparsers.add_parser('push')
+    pushp.add_argument('--prefix')
+    pushp.set_defaults(func=do_push)
+
+    pullp = subparsers.add_parser('pull')
+    pullp.add_argument('--prefix')
+    pullp.set_defaults(func=do_pull)
+
+    args = parser.parse_args()
+    args.func(args)
