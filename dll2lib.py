d232558ddaa244d3e4de50042283ab03f878e91d
maxint <lnychina@gmail.com>
2015-08-13 13:48:34 +0800

    Add destination directory

--- dll2lib.py
+++ dll2lib.py
@@ -32,15 +32,20 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Generate lib from dll')
     parser.add_argument('dll', action=readable, help='dll file path')
+    parser.add_argument('-d', '--dst_dir', default=None, help='destination directory')
 
     args = parser.parse_args()
 
+
+    if args.dst_dir is None:
+        args.dst_dir = os.path.dirname(args.dll)
+
     name = os.path.splitext(os.path.basename(args.dll))[0]
-    def_path = name + '.def'
-    lib_path = name + '.lib'
+    def_path = os.path.join(args.dst_dir, name + '.def')
+    lib_path = os.path.join(args.dst_dir, name + '.lib')
 
     print '[I] Generating ' + def_path
-    cmd = 'dumpbin /exports ' + args.dll
+    cmd = 'dumpbin /exports "{}"'.format(args.dll)
     out = runout(cmd)
 
     func_names = re.findall(r'\s+\d{1,2}\s+\d{1,2} [\dA-Z]{8} (\w+)', out)
@@ -48,7 +53,7 @@ if __name__ == '__main__':
         f.write('\n'.join(['EXPORTS'] + func_names))
 
     print '[I] Generating ' + lib_path
-    cmd = 'lib /def:{} /out:{}.lib'.format(def_path, lib_path)
+    cmd = 'lib /def:"{}" /out:"{}"'.format(def_path, lib_path)
     runout(cmd)
 
     print 'Done!'
